7e3c491b49913a7b1c86cec99bff77f8741e5ed6
popo <miss.poepoe23@gmail.com>
2019-11-20 14:38:37 +0100

    Home: responsive [part I]

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,21 +22,24 @@ const IndexPage = () => {
       <Layout>
         <SEO title="Home" />
         <div className="home">
-          <section className="head">
-            <ReadTheFirstArticle />  
-          </section>
-          <section className="description">
-            <div className="container">
-              <h4>Bringing digital interfaces to life with <br/> animation</h4>
-              <div className="line" />
-              <ul>
-                <li><p>How to use animation to solve problems ? </p></li>
-                <li><p>How to use animation for the power of good ?  </p></li>
-                <li><p>How to find a place for animation in a design process ? </p></li>
-                <li><p>How to animate responsibly ?  </p></li>
-                <li><p>How to give more personality and bring digital interfaces to life with subtile animation ?  </p></li>
-                <li><p>How to use interactive animation to tell a story ? </p></li>
-              </ul>
+          <section className="head" />
+          <section className="body" >
+            <div className="grid flex-end ">
+              <ReadTheFirstArticle />  
+            </div>
+            <div className="description">
+              <div className="grid">
+                <h4>Bringing digital interfaces to life with <br/> animation</h4>
+                <div className="line" />
+                <ul>
+                  <li><p>How to use animation to solve problems ? </p></li>
+                  <li><p>How to use animation for the power of good ?  </p></li>
+                  <li><p>How to find a place for animation in a design process ? </p></li>
+                  <li><p>How to animate responsibly ?  </p></li>
+                  <li><p>How to give more personality and bring digital interfaces to life with subtile animation ?  </p></li>
+                  <li><p>How to use interactive animation to tell a story ? </p></li>
+                </ul>
+              </div>
             </div>
           </section>
         </div>
