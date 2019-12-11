767e8f51eeb6fda68d10e8ee998f862afa81e99a
popo <miss.poepoe23@gmail.com>
2019-12-11 19:37:18 +0100

    Gatbsy node: next, previous

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,30 +1,49 @@
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
-    const result = await graphql(`
-      query {
-        allMdx {
-          nodes {
-            frontmatter {
-              slug
-            }
+  const result = await graphql(`
+    query {
+      allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+        nodes {
+          body
+          frontmatter {
+            slug
           }
         }
       }
-    `);
-  
-    if (result.errors) {
-      reporter.panic("failed to create posts", result.errors);
     }
-  
-    const posts = result.data.allMdx.nodes;
-  
-    posts.forEach(post => {
-      actions.createPage({
-        path: post.frontmatter.slug,
-        component: require.resolve("./src/templates/post.js"),
-        context: {
-          slug: post.frontmatter.slug
-        }
-      });
+  `)
+
+  if (result.errors) {
+    reporter.panic("failed to create posts", result.errors)
+  }
+
+  const posts = result.data.allMdx.nodes
+
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1]
+    const next = index === 0 ? null : posts[index - 1]
+
+    actions.createPage({
+      path: post.frontmatter.slug,
+      component: require.resolve("./src/templates/post.js"),
+      context: {
+        slug: post.frontmatter.slug,
+        previous,
+        next,
+      },
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `Mdx`) {
+    const value = createFilePath({ node, getNode });
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
     });
-  };
-  
+  }
+};
--- src/templates/post.js
+++ src/templates/post.js
@@ -3,7 +3,7 @@ import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import BackgroundImage from "gatsby-background-image"
 import Layout from "../components/layout"
-import {ReadTheNextArticle} from "../components/buttons"
+import { ReadTheNextArticle } from "../components/buttons"
 
 export const query = graphql`
   query($slug: String!) {
@@ -24,33 +24,35 @@ export const query = graphql`
   }
 `
 
-const PostTemplate = ({ data: { mdx: post } }) => (
-  <Layout>
-    <div className="post">
-      <BackgroundImage
-        Tag="section"
-        fluid={post.frontmatter.image.sharp.fluid}
-        backgroundColor={`#353434`}
-        fadeIn="soft"
-      >
-        <div className="grid">
-          <div className="post-head">
-            <h2 className="post-title">{post.frontmatter.title}</h2>
-            <div className="line" />
-            <h4 className="post-subtitle">{post.frontmatter.subtitle}</h4>
+const PostTemplate = ({ data: { mdx: post }, pageContext: {next, previous}}) => {
+  return (
+    <Layout>
+      <div className="post">
+        <BackgroundImage
+          Tag="section"
+          fluid={post.frontmatter.image.sharp.fluid}
+          backgroundColor={`#353434`}
+          fadeIn="soft"
+        >
+          <div className="grid">
+            <div className="post-head">
+              <h2 className="post-title">{post.frontmatter.title}</h2>
+              <div className="line" />
+              <h4 className="post-subtitle">{post.frontmatter.subtitle}</h4>
+            </div>
+            <div className="post-block">
+              <MDXRenderer>{post.body}</MDXRenderer>
+            </div>
           </div>
-          <div className="post-block">
-            <MDXRenderer>{post.body}</MDXRenderer>
-          </div>
-        </div>
-        <ReadTheNextArticle 
-          title='squash & stretch'
-          description='When applied, it gives your animated characters and objects the illusion ...'
-          linkTo={'/'}
-        />
-      </BackgroundImage>
-    </div>
-  </Layout>
-)
+          <ReadTheNextArticle
+            title="squash & stretch"
+            description="When applied, it gives your animated characters and objects the illusion ..."
+            linkTo={next.frontmatter.slug}
+          />
+        </BackgroundImage>
+      </div>
+    </Layout>
+  )
+}
 
 export default PostTemplate
