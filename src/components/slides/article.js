09e2996b298b0166b32714d0347932f8630fc38c
popo <miss.poepoe23@gmail.com>
2020-02-02 22:58:15 +0100

    Posts: Improving the User Experience Through Animation

--- src/components/slides/article.js
+++ src/components/slides/article.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import Title from "../title"
 
 const Article = ({ first, second, introduction, children }) => {
+  const oneColumn = introduction === undefined ? true : false;
   return (
     <div className="article grid">
       <div className="article-head">
@@ -12,8 +13,8 @@ const Article = ({ first, second, introduction, children }) => {
         />
       </div>
       <div className="article-container">
-        <h4 className="article-introduction">{introduction}</h4>
-        <div className="article-content">{children}</div>
+        {introduction && <h4 className="article-introduction">{introduction}</h4>}
+        <div className="article-content" data-single-column={oneColumn}>{children}</div>
       </div>
     </div>
   )
